22cd465f7f7d9cacb1c3abac0f7f166534f15244
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2023-04-30 12:28:31 +0200

    [doc] Update the readme

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,23 @@
-//! eelu-login is a command-line tool that allows users to log in to the EELU Moodle platform quickly and easily through the command-line interface.
+//! eelu-login is a command-line tool that allows stuff and students to log in to the The Egyptian E-Learning University(EELU) Moodle platform quickly and easily through the command-line interface.
+//! > This is a fork of [Crypt00o/eelu-login](https://github.com/Crypt00o/eelu-login) with more features and improvements and bugs =D
 //!
-//! ## Installation For Users
+//! # Installation
 //! You can install the latest stable version of eelu-login via Cargo:
-//! `cargo install eelu-login`
+//! ```sh
+//! cargo install eelu-login
+//! ```
 //!
 //! Or you can get the latest git version from the repository:
-//! `cargo install --git https://github.com/anaselgarhy/eelu-login.git`
-//!
-//! Or check [Releases page](https://github.com/Crypt00o/eelu-login/releases) for pre-built binaries (not updated frequently).
-//!
-//! ## Installation and Building For Developers
-//! To install eelu-login, you'll need to have Rust and Cargo installed on your machine. Once you have those installed, you can run the following command:
-//!
-//! build:
 //! ```sh
-//! cargo build --release
+//! cargo install --git https://github.com/anas-elgarhy/eelu-login.git
 //! ```
 //!
-//! run :
+//! Or you can install it from the AUR with your favorite AUR helper:
 //! ```sh
-//! cargo run
+//! yay -S eelu-login # or any other AUR helper you use
 //! ```
 //!
-//! ## Usage
+//! # Usage
 //! `eelu-login --help` will show you the usage of the tool:
 //! ```text
 //! [+] Usage : eelu-login [--user <username>] [--pass <password>] [--type <staff| sys-user | student>]
@@ -49,17 +44,10 @@
 //!
 //! You can don't need to specify the user, and the tool will be try to login as a student and if it fails it will try to login as a staff and if it fails it will try to login as a system user.
 //!
-//! ## Support
-//! If you have any questions or need help using eelu-login, you can contact the creator, Eslam Muhammad [0xCrypt00o], at 0xCrypt00o@protonmail.com. You can also support the creator by sending Bitcoin to the following address: bc1qdp3f6u3puwkyu6ztxz7hzcurd7jnjyw6hzjcas.
-//!
-//!
-//! ## Contributors 🦀❤
-//! - [Anas Elgarhy](https://github.com/anaselgarhy)
-//!
-//! ## Contributing
+//! # Contributing
 //! If you want to contribute to this project, please read the [contributing guidelines](./CONTRIBUTING.md) first.
 //!
-//! ## Useful Links
+//! # Useful Links
 //! - [sis-login crate](https://crates.io/crates/sis-login)
 //! - [EELU SIS](https://sis.eelu.edu.eg/)
 //! - [EELU Moodle](https://moodle.eelu.edu.eg/)
